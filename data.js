8e2bb33f1a4ff48c22c293b778dae87ad8fa7ba8
Steven Vore <steven@vorefamily.net>
2013-07-25 22:16:05 -0400

    moved data into separate file

---
+++ data.js
@@ -0,0 +1,76 @@
+var cars = [
+    { year: 2012, make: "Farrari", model: "California" },
+    { year: 2011, make: "BMW", model: "325is" },
+    { year: 2010, make: "Mini", model: "Cooper S" },
+    { year: 2012, make: "Chevy", model: "Cavalier" },
+    { year: 2011, make: "BMW", model: "R100RS" },
+    { year: 2010, make: "Ford", model: "Bronco" },
+    { year: 2012, make: "Dodge", model: "Dart" },
+    { year: 2010, make: "Volkswagen", model: "Thing" },
+    { year: 2012, make: "Volkswagen", model: "Super Beetle" },
+    { year: 2011, make: "Mini", model: "Clubman" },
+    { year: 2010, make: "Mini", model: "Countryman" },
+    { year: 2009, make: "Porsche", model: "Boxster" },
+    { year: 2013, make: "Porsche", model: "911" }
+];
+
+var makes = [
+    { make: "BMW"},
+    { make: "Chevy" },
+    { make: "Dodge" },
+    { make: "Farrari" },
+    { make: "Ford" },
+    { make: "Mini" },
+    { make: "Porche" },
+    { make: "Volkswagen" }
+];
+
+var BMWModels = [
+    { model: "325is" },
+    { model: "R100RS" }            
+];
+
+var ChevyModels = [
+    { model: "Cavalier" },            
+    { model: "Camaro" }            
+];
+
+var DodgeModels = [
+    { model: "Dart" },
+    { model: "Charger" }
+];
+
+var FerrariModels = [
+    { model: "F12berlinetta" },
+    { model: "California" }
+];
+
+var FordModels = [
+    { model: "Mustang" },
+    { model: "Fiesta" }
+];
+
+var MiniModels = [
+    { model: "Cooper" },
+    { model: "Cooper S" },
+    { model: "Clubman" },
+    { model: "Countryman" } 
+];
+
+var PorscheModels = [
+    { model: "Boxster" },
+    { model: "911" }
+];
+
+var VolkswagenModels = [
+    { model: "Beetle" },
+    { model: "Thing" }
+];
+
+var Colors = [
+    { color: "Red" },
+    { color: "White" },
+    { color: "Blue" },
+    { color: "Black" }
+];
+
