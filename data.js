27edc34606dda75be729251663117a31d217caf1
Steven Vore <steven@vorefamily.net>
2013-07-31 12:51:25 -0400

    split data into individual arrays

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 var cars = [
-    { year: 2012, make: "Farrari", model: "California" },
+    { year: 2012, make: "Ferrari", model: "California" },
     { year: 2011, make: "BMW", model: "325is" },
     { year: 2010, make: "Mini", model: "Cooper S" },
     { year: 2012, make: "Chevy", model: "Cavalier" },
@@ -14,63 +14,65 @@ var cars = [
     { year: 2013, make: "Porsche", model: "911" }
 ];
 
-var makes = [
-    { make: "BMW"},
-    { make: "Chevy" },
-    { make: "Dodge" },
-    { make: "Farrari" },
-    { make: "Ford" },
-    { make: "Mini" },
-    { make: "Porche" },
-    { make: "Volkswagen" }
+var emptyList = [];
+
+var makedata = [
+    { text: "BMW"        , value:"1"},
+    { text: "Chevy"      , value:"2"},
+    { text: "Dodge"      , value:"3"},
+    { text: "Ferrari"    , value: "4" },
+    { text: "Ford"       , value:"5"},
+    { text: "Mini"       , value:"6"},
+    { text: "Porsche"    , value:"7"},
+    { text: "Volkswagen" , value:"8"}
 ];
 
 var BMWModels = [
-    { model: "325is" },
-    { model: "R100RS" }            
+    { model: "325is" , value: "1" },
+    { model: "R100RS", value: "2" }
 ];
 
 var ChevyModels = [
-    { model: "Cavalier" },            
-    { model: "Camaro" }            
+    { model: "Cavalier", value: "1" },
+    { model: "Camaro"  , value: "2" }
 ];
 
 var DodgeModels = [
-    { model: "Dart" },
-    { model: "Charger" }
+    { model: "Dart"    , value: "1"},
+    { model: "Charger" , value: "2" }
 ];
 
 var FerrariModels = [
-    { model: "F12berlinetta" },
-    { model: "California" }
+    { model: "F12berlinetta", value: "1" },
+    { model: "California"   , value: "2" }
 ];
 
 var FordModels = [
-    { model: "Mustang" },
-    { model: "Fiesta" }
+    { model: "Mustang", value: "1" },
+    { model: "Fiesta" , value: "2" }
 ];
 
 var MiniModels = [
-    { model: "Cooper" },
-    { model: "Cooper S" },
-    { model: "Clubman" },
-    { model: "Countryman" } 
+    { model: "Cooper"    , value: "1" },
+    { model: "Cooper S"  , value: "2" },
+    { model: "Clubman"   , value: "3" },
+    { model: "Countryman", value: "4" }
 ];
 
 var PorscheModels = [
-    { model: "Boxster" },
-    { model: "911" }
+    { model: "Boxster", value: "1" },
+    { model: "911"    , value: "2" }
 ];
 
 var VolkswagenModels = [
-    { model: "Beetle" },
-    { model: "Thing" }
+    { model: "Beetle", value: "1" },
+    { model: "Thing" , value: "2" }
 ];
 
 var Colors = [
-    { color: "Red" },
-    { color: "White" },
-    { color: "Blue" },
-    { color: "Black" }
+    { color: "Red"  , value: "1" },
+    { color: "White", value: "2" },
+    { color: "Blue" , value: "3" },
+    { color: "Black", value: "4" }
 ];
 
