20e52d853229d7ec29c99828e90379ce5f91ef3d
StevenJV <steven@vorefamily.net>
2013-12-09 15:27:22 -0500

    added node.js files for heroku

---
+++ web.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+var Server = require('mongodb').Server;
+var port = process.env.PORT || 5000;
+
+app.configure(function(){
+  app.use(express.bodyParser());
+});
+
+app.use(express.logger());
+
+app.use("/", express.static("content"));
+
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
