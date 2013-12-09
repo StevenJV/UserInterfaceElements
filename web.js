4eb6e1c45d7901e708318bd61e87c14a3d2071a1
StevenJV <steven@vorefamily.net>
2013-12-09 15:34:11 -0500

    heroku

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var Server = require('mongodb').Server;
+//var Server = require('mongodb').Server;
 var port = process.env.PORT || 5000;
 
 app.configure(function(){
@@ -9,7 +9,7 @@ app.configure(function(){
 
 app.use(express.logger());
 
-app.use("/", express.static("content"));
+//app.use("/", express.static("content"));
 
 app.listen(port, function() {
   console.log("Listening on " + port);
